57ca3579a30572eff2aadfe489409e30d19b9e7b
Josh Koenig <josh@getpantheon.com>
2011-08-30 18:39:15 +0100

    adding a status-code setting value

--- misc/healthchecks/db.check.php
+++ misc/healthchecks/db.check.php
@@ -5,11 +5,20 @@ if (isset($_SERVER['PRESSFLOW_CONFIG'])) {
   $db = $pressflow_config['databases']['default']['default'];
   $link = mysql_connect($db['host'] . ':' . $db['port'], $db['username'], $db['password']);
   if (!$link) {
-      die('Could not connect: ' . mysql_error());
+      fail('Could not connect: ' . mysql_error());
   }
-  echo "Connected successfully\n";
+  echo "OK\n";
   mysql_close($link);
 }
 else {
-  die("NO CONFIG FOUND\n");
+  fail("No config found.\n");
+}
+
+/**
+ * Fail with a status code.
+ */
+function fail($message, $code = 500) {
+  header(sprintf("HTTP/1.0 %s %s", $code, $message));
+  echo $message;
+  exit;
 }
