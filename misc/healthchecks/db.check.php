bef8634668f4ac24081fae98c5bd4f15ba633c89
Josh Koenig <josh@getpantheon.com>
2011-09-21 15:03:21 -0700

    moving to PRESSFLOW_SETTINGS

--- misc/healthchecks/db.check.php
+++ misc/healthchecks/db.check.php
@@ -1,7 +1,7 @@
 <?php
 // Load environmental config, if present.
-if (isset($_SERVER['PRESSFLOW_CONFIG'])) {
-  $pressflow_config = json_decode($_SERVER['PRESSFLOW_CONFIG'], TRUE);
+if (isset($_SERVER['PRESSFLOW_SETTINGS'])) {
+  $pressflow_config = json_decode($_SERVER['PRESSFLOW_SETTINGS'], TRUE);
   $db = $pressflow_config['databases']['default']['default'];
   $link = mysql_connect($db['host'] . ':' . $db['port'], $db['username'], $db['password']);
   if (!$link) {
