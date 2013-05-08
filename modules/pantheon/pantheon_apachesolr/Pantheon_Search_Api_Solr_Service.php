09ca71f6ef305977c2ad207f29c74f3fb2602b2e
Matt Cheney <matt@getpantheon.com>
2013-05-08 12:45:33 -0700

    Updated support for new service class name for Search API RC4. This uses hook_search_api_service_info_alter() to add the class and maintains legacy support for RC2

--- modules/pantheon/pantheon_apachesolr/Pantheon_Search_Api_Solr_Service.php
+++ modules/pantheon/pantheon_apachesolr/Pantheon_Search_Api_Solr_Service.php
@@ -1,4 +1,61 @@
 <?php
+
+/**
+ * Current Supported Class for RC4
+ */
+class PantheonApachesolrSearchApiSolrConnection extends SearchApiSolrConnection {
+
+  function __construct(array $options) {
+
+    // Adding in custom settings for Pantheon
+    $options['scheme'] = 'https';
+    $options['host'] = (variable_get('pantheon_hyperion_host')) ? variable_get('pantheon_hyperion_host') : 'index.' . variable_get('pantheon_tier', 'live') . '.getpantheon.com';
+    $options['path'] = 'sites/self/environments/' . variable_get('pantheon_environment', 'dev') . '/index';
+    $options['port'] = 449;
+    $this->context = stream_context_create(
+      array(
+        'ssl' => array(
+           'local_cert' => '../certs/binding.pem',
+         )
+      )
+    );
+
+    // Adding in general settings for Search API
+    $options += array(
+      'scheme' => 'http',
+      'host' => 'localhost',
+      'port' => 8983,
+      'path' => 'solr',
+      'http_user' => NULL,
+      'http_pass' => NULL,
+      'http_method' => 'POST',
+      'local_cert'=> NULL,
+    );
+    $this->options = $options;
+
+    $path = '/' . trim($options['path'], '/') . '/';
+    $this->base_url = $options['scheme'] . '://' . $options['host'] . ':' . $options['port'] . $path;
+
+    // Make sure we always have a valid method set, default to POST.
+    $this->method = $options['http_method'] == 'GET' ? 'GET' : 'POST';
+
+    // Set HTTP Basic Authentication parameter, if login data was set.
+    if (strlen($options['http_user']) && strlen($options['http_pass'])) {
+      $this->http_auth = 'Basic ' . base64_encode($options['http_user'] . ':' . $options['http_pass']);
+    }
+  }
+
+}
+
+class PantheonApachesolrSearchApiSolrService extends SearchApiSolrService {
+
+  protected $connection_class = 'PantheonApachesolrSearchApiSolrConnection';
+
+}
+
+/**
+ * Legacy Supported Class for RC2
+ */
 class PantheonSearchApiSolrService extends SearchApiSolrConnection {
 
   /**
