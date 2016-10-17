9e74cb887a6f743079e04be35997a1a46aac34f0
Herb vd Dool <herb@freeform.ca>
2016-10-17 17:10:20 -0400

    security updates ctools 7.x-1.10, views 7.x-3.14

--- profiles/civicrm_starterkit/modules/contrib/ctools/js/dependent.js
+++ profiles/civicrm_starterkit/modules/contrib/ctools/js/dependent.js
@@ -40,7 +40,7 @@
   Drupal.CTools.dependent.autoAttach = function() {
     // Clear active bindings and triggers.
     for (i in Drupal.CTools.dependent.activeTriggers) {
-      $(Drupal.CTools.dependent.activeTriggers[i]).unbind('change');
+      $(Drupal.CTools.dependent.activeTriggers[i]).unbind('change.ctools-dependent');
     }
     Drupal.CTools.dependent.activeTriggers = [];
     Drupal.CTools.dependent.activeBindings = {};
@@ -97,7 +97,13 @@
           else {
             switch ($(trigger).attr('type')) {
               case 'checkbox':
-                var val = $(trigger).attr('checked') ? true : false;
+                // **This check determines if using a jQuery version 1.7 or newer which requires the use of the prop function instead of the attr function when not called on an attribute
+                if ($().prop) {
+                  var val = $(trigger).prop('checked') ? true : false;
+                }
+                else {
+                  var val = $(trigger).attr('checked') ? true : false;
+                }
 
                 if (val) {
                   $(trigger).siblings('label').removeClass('hidden-options').addClass('expanded-options');
@@ -148,34 +154,41 @@
                 len++;
               }
 
-              var object = $('#' + id + '-wrapper');
-              if (!object.size()) {
-                // Some elements can't use the parent() method or they can
-                // damage things. They are guaranteed to have wrappers but
-                // only if dependent.inc provided them. This check prevents
-                // problems when multiple AJAX calls cause settings to build
-                // up.
-                var $original = $('#' + id);
-                if ($original.is('fieldset') || $original.is('textarea')) {
-                  continue;
-                }
-
-                object = $('#' + id).parent();
+              var $original = $('#' + id);
+              if ($original.is('fieldset') || $original.is('textarea')) {
+                continue;
               }
+
+              var object = $original.parent();
 
               if (Drupal.settings.CTools.dependent[id].type == 'disable') {
                 if (Drupal.settings.CTools.dependent[id].num <= len) {
                   // Show if the element if criteria is matched
-                  object.attr('disabled', false);
-                  object.addClass('dependent-options');
-                  object.children().attr('disabled', false);
+                  // **This check determines if using a jQuery version 1.7 or newer which requires the use of the prop function instead of the attr function when not called on an attribute
+                  if (typeof $().prop == 'function') {
+                    object.prop('disabled', false);
+                    object.addClass('dependent-options');
+                    object.children().prop('disabled', false);
+                  }
+                  else {
+                    object.attr('disabled', false);
+                    object.addClass('dependent-options');
+                    object.children().attr('disabled', false);
+                  }
                 }
                 else {
                   // Otherwise hide. Use css rather than hide() because hide()
                   // does not work if the item is already hidden, for example,
                   // in a collapsed fieldset.
-                  object.attr('disabled', true);
-                  object.children().attr('disabled', true);
+                  // **This check determines if using a jQuery version 1.7 or newer which requires the use of the prop function instead of the attr function when not called on an attribute
+                  if (typeof $().prop == 'function') {
+                    object.prop('disabled', true);
+                    object.children().prop('disabled', true);
+                  }
+                  else {
+                    object.attr('disabled', true);
+                    object.children().attr('disabled', true);
+                  }
                 }
               }
               else {
@@ -194,7 +207,7 @@
             }
           }
 
-          $(trigger_id).change(function() {
+          $(trigger_id).bind('change.ctools-dependent', function() {
             // Trigger the internal change function
             // the attr('id') is used because closures are more confusing
             changeTrigger(trigger_id, bind_id);
@@ -215,7 +228,7 @@
       // is a sort of hacked one that's faster but much less flexible.
       $("select.ctools-master-dependent")
         .once('ctools-dependent')
-        .change(function() {
+        .bind('change.ctools-dependent', function() {
           var val = $(this).val();
           if (val == 'all') {
             $('.ctools-dependent-all').show(0);
@@ -225,7 +238,7 @@
             $('.ctools-dependent-' + val).show(0);
           }
         })
-        .trigger('change');
+        .trigger('change.ctools-dependent');
     }
   }
 })(jQuery);
